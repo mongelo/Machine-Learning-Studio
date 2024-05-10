ea57f671a6a79aad61a98021f7f1831a53efab05
mongelo <mongelonas@hotmail.com>
2024-05-10 11:28:20 +0200

    buttons are cleaner and clickable

--- StartPage/MainPage.xaml.cs
+++ StartPage/MainPage.xaml.cs
@@ -1,12 +1,18 @@
-﻿namespace Machine_Learning_Studio
+﻿using System.Windows.Input;
+
+namespace Machine_Learning_Studio
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        public ICommand Button1Command { get; }
+        public ICommand Button2Command { get; }
 
         public MainPage()
         {
             InitializeComponent();
+            Button1Command = new Command(ExecuteButton1Command);
+            Button2Command = new Command(ExecuteButton2Command);
         }
 
         private void OnCounterClicked(object sender, EventArgs e)
@@ -19,6 +25,18 @@
                 CounterBtn.Text = $"Clicked {count} times";
 
             SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+
+        private void ExecuteButton1Command()
+        {
+            Console.WriteLine("Button 1 clicked");
+            CounterBtn.Text = $"Clime";
+        }
+
+        private void ExecuteButton2Command()
+        {
+            Console.WriteLine("Button 2 clicked");
+            CounterBtn.Text = $"Clime";
         }
     }
 
--- StartPage/MenuButton.xaml.cs
+++ StartPage/MenuButton.xaml.cs
@@ -1,9 +1,15 @@
+using System.Windows.Input;
+
 namespace Machine_Learning_Studio.StartPage;
 
-public partial class MenuButton : ContentView
+public partial class MenuButton
 {
+
     public static readonly BindableProperty ButtonTextProperty =
             BindableProperty.Create(nameof(ButtonText), typeof(string), typeof(MenuButton), default(string));
+
+    public static readonly BindableProperty CommandProperty =
+        BindableProperty.Create(nameof(Command), typeof(ICommand), typeof(MenuButton), null);
 
     public string ButtonText
     {
@@ -11,9 +17,21 @@ public partial class MenuButton : ContentView
         set { SetValue(ButtonTextProperty, value); }
     }
 
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+
     public MenuButton()
     {
         InitializeComponent();
         BindingContext = this;
     }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        Command?.Execute(null);
+    }
+
 }
